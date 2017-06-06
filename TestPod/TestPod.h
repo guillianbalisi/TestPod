2c32ad2e0bf9f43044eea2f3c3428d6655b8524c
Guillian Balisi <guillianbalisi@gmail.com>
2017-06-06 10:24:38 -0400

    Initial Commit

---
+++ TestPod/TestPod.h
@@ -0,0 +1,19 @@
+//
+//  TestPod.h
+//  TestPod
+//
+//  Created by Guillian Balisi on 2017-06-06.
+//  Copyright © 2017 Guillian Balisi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestPod.
+FOUNDATION_EXPORT double TestPodVersionNumber;
+
+//! Project version string for TestPod.
+FOUNDATION_EXPORT const unsigned char TestPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestPod/PublicHeader.h>
+
+
